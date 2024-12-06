e13859e012c4e2620232438c8e37ad8fe86b234d
Yehor Popovych <yehor.popovych@datadoghq.com>
2024-12-06 22:33:01 +0000

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let releaseVersion = "0.0.0"
+let relaseChecksum = "aaaaaaaa"
+let url = "https://github.com/DataDog/swift-code-coverage/releases/download/\(releaseVersion)/CodeCoverage.zip"
+
+let package = Package(
+    name: "swift-code-coverage",
+    platforms: [.macOS(.v10_13), .macCatalyst(.v14), .iOS(.v11), .tvOS(.v11)],
+    products: [
+        .library(name: "CodeCoverage",
+                 targets: ["CodeCoverage"]),
+    ],
+    targets: [
+        .binaryTarget(name: "CodeCoverage",
+                      url: url,
+                      checksum: relaseChecksum)
+    ]
+)
