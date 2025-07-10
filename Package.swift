5dae25c32ba4144d8f3b7971ccb1c2a517efd0c6
runner <runner@Mac-1752144565570.local>
2025-07-10 11:19:25 +0000

    Updated binary package version to 1.2.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let releaseVersion = "1.1.1"
-let relaseChecksum = "15e5f449ad28605d8006db42e162731fcc21fb2a16a0d3956022cfbdb4e9ad08"
+let releaseVersion = "1.2.0"
+let relaseChecksum = "6fedb29929e454764583dfeb54fdffa47b76c8ca25474a8c9867aac466d7cb98"
 let url = "https://github.com/DataDog/swift-code-coverage/releases/download/\(releaseVersion)/CodeCoverage.zip"
 
 let package = Package(
